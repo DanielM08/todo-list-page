71054c232a6515673b05ec19afc91f78794271f4
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-10-13 09:04:17 -0300

    Adding states to components

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,22 +1,43 @@
 import styles from './Task.module.css';
 import { Circle, CheckCircle, Trash } from 'phosphor-react';
 
-interface TaskProps {
-  disabled?: boolean;
+interface ITask {
+  content: string;
+  concluded: boolean;
 }
 
-export function Task({ disabled = true }: TaskProps) {
+interface TaskProps {
+  task: ITask;
+  onDeleteTask: (task: ITask) => void;
+  onTaskConcluded: (task: ITask) => void;
+}
+
+export function Task({ task, onDeleteTask, onTaskConcluded }: TaskProps) {
+  
+  function handleTaskConcluded(){
+    onTaskConcluded(task);
+  }
+
+  function handleDeleteTask(){
+    onDeleteTask(task);
+  }
+
   return (
     <div className={styles.task}>
-      <button type={"submit"} disabled={disabled} className={styles.check}>
-        {disabled ? 
-          <Circle size={17.45} color={'#4EA8DE'} /> : 
-          <CheckCircle size={17.45} weight="fill" color={'#5E60CE'}/>
+      <button 
+        onClick={handleTaskConcluded}
+        type={"submit"}
+        disabled={task.concluded}
+        className={styles.check}
+      >
+        {task.concluded ? 
+          <CheckCircle size={17.45} weight="fill" color={'#5E60CE'}/> : 
+          <Circle size={17.45} color={'#4EA8DE'} />
         }
-        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+        {task.content}
       </button>
 
-      <button className={styles.trash}>
+      <button onClick={handleDeleteTask} className={styles.trash}>
         <Trash size={14} color={'#808080'} />
       </button>
     </div>
--- src/components/TaskPanel.tsx
+++ src/components/TaskPanel.tsx
@@ -2,17 +2,76 @@ import styles from './TaskPanel.module.css';
 import { PlusCircle } from 'phosphor-react';
 import { EmptyList } from './EmptyList';
 import { Task } from './Task';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+
+interface ITask {
+  content: string;
+  concluded: boolean;
+}
 
 export function TaskPanel() {
+
+  const [taskList, setTaskList] = useState([] as ITask[]);
+  const [newTaskText, setNewTaskText] = useState('');
+
+  const [numConcludedTasks, setNumConcludedTasks] = useState(0);
+
+  function handleCreateNewTask(event: FormEvent){
+    event.preventDefault()
+
+    const newTask = {
+      content: newTaskText,
+      concluded: false,
+    }
+    setTaskList([...taskList, newTask]);
+    setNewTaskText('');
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>){
+    event.target.setCustomValidity('');
+    setNewTaskText(event.target.value);
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLTextAreaElement>){
+    event.target.setCustomValidity('Este campo é obrigatório');
+  }
+
+  function deleteTask(task: ITask){
+    const taskListWithoutDeletedOne = taskList.filter(t => t.content !== task.content);
+    setTaskList([...taskListWithoutDeletedOne]);
+
+    if(task.concluded){
+      setNumConcludedTasks(numConcludedTasks - 1);
+    }
+  }
+
+  function concludeTask(task: ITask){
+    const taskListWithConcludedOne = taskList.map(t => {
+      if(t.content === task.content){
+        t.concluded = !task.concluded;
+      }
+      return t;
+    });
+
+    setTaskList([...taskListWithConcludedOne]);
+    setNumConcludedTasks(numConcludedTasks + 1);
+  }
+
+  const isNewTaskEmpty = newTaskText.length === 0;
+
   return(
     <div>
-      <form className={styles.addTaskForm}>
+      <form onSubmit={handleCreateNewTask} className={styles.addTaskForm}>
         <textarea 
           name='Criar'
           placeholder='Adicione uma nova tarefa'
+          value={newTaskText}
+          onChange={handleNewTaskChange}
+          onInvalid={handleNewTaskInvalid}
+          required
         />
         
-        <button type='submit'>
+        <button type='submit' disabled={isNewTaskEmpty}>
           Criar
           <PlusCircle size={18}/>
         </button>
@@ -23,22 +82,29 @@ export function TaskPanel() {
         <header>
           <div className={styles.createdTasksCounter} >
             <strong>Tarefas criadas</strong>
-            <span>0</span>
+            <span>{taskList.length}</span>
           </div>
 
           <div className={styles.concludedTasksCounter}>
             <strong>Concluídas</strong>
-            <span>2 de 5</span>
+            <span>{`${numConcludedTasks} de ${taskList.length}`}</span>
           </div>
         </header>
       </div>
 
-      {/* <EmptyList /> */}
-      <Task />
-      <Task />
-      <Task disabled={false}/>
-      <Task disabled={false}/>
-      <Task disabled={false}/>
+      <div>
+        {
+          taskList.length === 0 ? <EmptyList /> :
+          taskList.map(t => 
+            <Task 
+              key = {t.content}
+              task = {t}
+              onDeleteTask = {deleteTask}
+              onTaskConcluded = {concludeTask}
+            />  
+          )
+        }
+      </div>
     </div>
   );
 }
